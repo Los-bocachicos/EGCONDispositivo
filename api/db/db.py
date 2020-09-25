14dfaf823f245d70af2fd2017510e56095b93215
Mateo Arboleda <marteomasoyyo@gmail.com>
2020-09-25 14:45:25 -0500

    Change all database connection variables to env variables

--- api/db/db.py
+++ api/db/db.py
@@ -2,10 +2,10 @@ import mysql.connector as mysql
 import os
 
 cnx = mysql.MySQLConnection(
-    host="172.17.0.2",
-    port=3306,
-    user="root",
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT'),
+    user=os.getenv('DB_USER'),
     password=os.getenv('DB_PASSWORD'),
-    database="EGCON",
+    database=os.getenv('DB_NAME'),
     auth_plugin='mysql_native_password'
 )
